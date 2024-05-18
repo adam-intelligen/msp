fb8ec88482055c6faf334d7c777e8d2ba24f89e7
adam-intelligen <100817445+adam-intelligen@users.noreply.github.com>
2024-05-18 13:45:15 +1000

    Update msp.py

--- msp.py
+++ msp.py
@@ -8,10 +8,17 @@ DATA_URL = ('https://am-bucket-123.s3.ap-southeast-2.amazonaws.com/'
          'test/MSP.csv')
 
 @st.cache_data
-def load_data:
+def load_data():
     data = pd.read_csv(DATA_URL)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+data = load_data()
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("Done! (using st.cache_data)")
+
 
 st.map(data)
